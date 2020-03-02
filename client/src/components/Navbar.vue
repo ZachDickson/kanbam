9ce68e0fc18b01b26b0b523d497ae06e5c54cbd5
BradleyG248 <calvinandhobbs28@gmail.com>
2020-03-02 14:40:07 -0700

    Added a theme

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -59,4 +59,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+nav {
+  font-family: cursive;
+}
+</style>
--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,41 +1,63 @@
 <template>
-  <div class="boards">
-    WELCOME TO THE BOARDS!!!
-    <form @submit.prevent="addBoard">
-      <input type="text" placeholder="title" v-model="newBoard.title" required>
-      <input type="text" placeholder="description" v-model="newBoard.description">
-      <button type="submit">Create Board</button>
-    </form>
-    <div v-for="board in boards" :key="board._id">
-      <router-link :to="{name: 'board', params: {boardId: board._id}}">{{}}">{{board.title}}</router-link>
+  <div class="bg-dark">
+    <div class="container">
+      <div class="row">
+        <div class="boards col-12">
+          <h1></h1>
+          <form @submit.prevent="addBoard">
+            <input
+              class="rounded-pill border-0 height-2 form-control form-control-lg"
+              type="text"
+              placeholder="title"
+              v-model="newBoard.title"
+              required
+            />
+            <input
+              class="rounded-pill border-0 height-2 form-control form-control-lg"
+              type="text"
+              placeholder="description"
+              v-model="newBoard.description"
+            />
+            <button class="btn btn-info" type="submit">Create Board</button>
+          </form>
+          <div v-for="board in boards" :key="board._id">
+            <router-link :to="{name: 'board', params: {boardId: board._id}}">>{{board.title}}</router-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "boards",
-    mounted() {
-      this.$store.dispatch("getBoards");
-    },
-    data() {
-      return {
-        newBoard: {
-          title: "",
-          description: ""
-        }
-      };
-    },
-    computed: {
-      boards() {
-        return this.$store.state.boards;
+export default {
+  name: "boards",
+  mounted() {
+    this.$store.dispatch("getBoards");
+  },
+  data() {
+    return {
+      newBoard: {
+        title: "",
+        description: ""
       }
-    },
-    methods: {
-      addBoard() {
-        this.$store.dispatch("addBoard", this.newBoard);
-        this.newBoard = { title: "", description: "" };
-      }
+    };
+  },
+  computed: {
+    boards() {
+      return this.$store.state.boards;
     }
-  };
+  },
+  methods: {
+    addBoard() {
+      this.$store.dispatch("addBoard", this.newBoard);
+      this.newBoard = { title: "", description: "" };
+    }
+  }
+};
 </script>
+<style>
+div.bg-dark {
+  min-height: 92vh;
+}
+</style>
