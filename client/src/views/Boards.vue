c8bf18600e912e7f690344b1e02c7b67bd047eb0
BradleyG248 <calvinandhobbs28@gmail.com>
2020-03-02 16:05:24 -0700

    Worked on the boards view

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="bg-dark">
+  <div class="bg-dark text-success">
+    <button class="btn btn-success" v-show="!create" @click="create = !create">Create Board</button>
     <div class="container">
-      <div class="row">
-        <div class="boards col-12">
-          <h1></h1>
-          <form @submit.prevent="addBoard">
+      <div v-show="create" class="row">
+        <div class="boards col-12 col-md-6 offset-md-3 pb-2">
+          <h1 class="border-bottom border-success">Create a Board!</h1>
+          <form class="create-board" @submit.prevent="addBoard">
             <input
-              class="rounded-pill border-0 height-2 form-control form-control-lg"
+              class="rounded-pill border-0 height-2 form-control form-control-default mb-5 mt-3 p-2 border-bottom border-success"
               type="text"
               placeholder="title"
               v-model="newBoard.title"
               required
             />
             <input
-              class="rounded-pill border-0 height-2 form-control form-control-lg"
+              class="rounded-pill border-0 height-2 form-control form-control-default mb-5 p-2"
               type="text"
               placeholder="description"
               v-model="newBoard.description"
             />
-            <button class="btn btn-info" type="submit">Create Board</button>
+            <button class="btn btn-light" type="submit">Create Board</button>
+            <button class="btn btn-warning" @click="create=!create">Cancel</button>
           </form>
           <div v-for="board in boards" :key="board._id">
             <router-link :to="{name: 'board', params: {boardId: board._id}}">>{{board.title}}</router-link>
@@ -40,7 +42,8 @@ export default {
       newBoard: {
         title: "",
         description: ""
-      }
+      },
+      create: false
     };
   },
   computed: {
@@ -49,15 +52,26 @@ export default {
     }
   },
   methods: {
-    addBoard() {
-      this.$store.dispatch("addBoard", this.newBoard);
+    async addBoard() {
+      await this.$store.dispatch("addBoard", this.newBoard);
       this.newBoard = { title: "", description: "" };
+      this.create = false;
+      console.log("FALSE");
     }
+  },
+  components: {
+    Board
   }
 };
 </script>
 <style>
 div.bg-dark {
   min-height: 92vh;
+  font-family: cursive;
+}
+div.boards {
+  margin-top: 10%;
+  border: 0.1rem solid #4f48db;
+  border-top: 2rem solid #4f48db;
 }
 </style>
