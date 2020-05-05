e49a3953443d8e70609325dca93e9e65c054a08f
Zach Dickson <z.dickson10@gmail.com>
2020-05-05 14:32:39 -0600

    index

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 require("dotenv").config();
 require = require("esm")(module);
 module.exports = require("./server/main");
+
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
