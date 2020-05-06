23f4d49038dbfbadcdb1ee675b513b325de2e696
Zach Dickson <z.dickson10@gmail.com>
2020-05-06 14:59:15 -0600

    sockets

--- client/src/store/socketStore.js
+++ client/src/store/socketStore.js
@@ -1,46 +1,46 @@
-import io from "socket.io-client";
+// import io from "socket.io-client";
 
-let socket = {};
+// let socket = {};
 
-export const socketStore = {
-  actions: {
-    initializeSocket({ commit, dispatch }) {
-      //establish connection with socket
-      socket = io("//localhost:3000");
-      //Handle any on connection events
-      socket.on("CONNECTED", data => {
-        console.log("Connected to socket, villany may commence");
-      });
+// export const socketStore = {
+//   actions: {
+//     initializeSocket({ commit, dispatch }) {
+//       //establish connection with socket
+//       socket = io("//localhost:3000");
+//       //Handle any on connection events
+//       socket.on("CONNECTED", data => {
+//         console.log("Connected to socket, villany may commence");
+//       });
 
-      //register all listeners
-      socket.on("createList", item => {
-        commit("addList", item);
-      });
+//       //register all listeners
+//       socket.on("createList", item => {
+//         commit("addList", item);
+//       });
 
-      //register all listeners
+//       //register all listeners
 
-      socket.on("deleteList", data => {
-        commit("deleteList", data);
-      });
+//       socket.on("deleteList", data => {
+//         commit("deleteList", data);
+//       });
 
-      //TASKS
+//       //TASKS
 
-      socket.on("createTask", data => {
-        commit("addTask", data);
-      });
+//       socket.on("createTask", data => {
+//         commit("addTask", data);
+//       });
 
-      socket.on('removeTask', data => {
-        // debugger;
-        commit("deleteTask", data);
-      });
+//       socket.on('removeTask', data => {
+//         // debugger;
+//         commit("deleteTask", data);
+//       });
 
-      socket.on("changeTask", data => {
-        commit("editTask", data);
-      });
-    },
+//       socket.on("changeTask", data => {
+//         commit("editTask", data);
+//       });
+//     },
 
-    joinRoom({ commit, dispatch }, roomName) {
-      socket.emit("dispatch", { action: "JoinRoom", data: roomName });
-    }
-  }
-};
+//     joinRoom({ commit, dispatch }, roomName) {
+//       socket.emit("dispatch", { action: "JoinRoom", data: roomName });
+//     }
+//   }
+// };
