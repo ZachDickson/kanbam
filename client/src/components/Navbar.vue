a618b6034583d5806c02893e855fd34b138134e5
Zach Dickson <z.dickson10@gmail.com>
2020-05-05 18:59:15 -0600

    nav

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand" :to="{ name: 'home' }">Kanban</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'home' }">Kanbam!</router-link>
     <button
       class="navbar-toggler"
       type="button"
